9450c311b1d3b1719de98a2f0efaf00911694462
garibere13 <gari.bereciartua@opendeusto.es>
2016-11-12 17:38:33 +0100

    he puesto la pantalla principal de la aplicación un poco más bonita

--- Ajedrez (Mariano)/src/GUI/clsPaginaPrincipal.java
+++ Ajedrez (Mariano)/src/GUI/clsPaginaPrincipal.java
@@ -1,11 +1,13 @@
 package GUI;
 
+
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.EventQueue;
+import java.awt.Image;
 
+import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
-import javax.swing.JDesktopPane;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -14,8 +16,10 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.border.Border;
+
 import LN.clsGestor;
 import LN.clsUsuario;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.util.ArrayList;
@@ -32,30 +36,13 @@ public class clsPaginaPrincipal extends JFrame {
 	private JLabel lblLogin;
 	private Border borderUsuario;
 	private Border borderContraseña;
-
-
+	private JLabel img;
+	
+	final int posImgX=440;
+	final int posImgY=120;
 
 	ArrayList<clsUsuario> usus=new ArrayList<clsUsuario>();
 
-
-
-
-//	/**
-//	 * Launch the application.
-//	 */
-//	public static void main(String[] args) {
-//		EventQueue.invokeLater(new Runnable() {
-//			public void run() {
-//				try {
-//					clsPaginaPrincipal window = new clsPaginaPrincipal();
-//					window.setVisible(true);
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//				}
-//			}
-//		});
-//	}
-	
 
 	/**
 	 * Create the application.
@@ -63,11 +50,14 @@ public class clsPaginaPrincipal extends JFrame {
 	public clsPaginaPrincipal() {
 	
 
+		
+		
 		panel=new JPanel();
-		this.setBounds(450, 225, 450, 300);
+//		this.setBounds(450, 225, 450, 300);
+		this.setBounds(350, 200, 500, 300);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		panel.setLayout(null);
-		add( panel, BorderLayout.CENTER );
+		getContentPane().add( panel, BorderLayout.CENTER );
 		setTitle("¡Bienvenido al Ajedrez Mariano!");
 		panel.setBackground(Color.WHITE);
 		
@@ -94,22 +84,29 @@ public class clsPaginaPrincipal extends JFrame {
 		panel.add(lblContraseña);
 		
 		btnAceptar = new JButton("Entrar");
-		btnAceptar.setBounds(269, 199, 89, 23);
+		btnAceptar.setBounds(286, 199, 89, 23);
 		panel.add(btnAceptar);
 		
 		btnRegistro = new JButton("¡Regístrate ahora!");
-		btnRegistro.setBounds(252, 110, 172, 23);
+		btnRegistro.setBounds(267, 110, 172, 23);
 		panel.add(btnRegistro);
 		
 		lblLogin = new JLabel("¿Te atreves a jugar contra Mariano?");
-		lblLogin.setBounds(70, 21, 251, 53);
+		lblLogin.setBounds(27, 0, 573, 75);
+		lblLogin.setFont (lblLogin.getFont ().deriveFont (24.0f));
 		panel.add(lblLogin);
 		
 		
 		
 		
+		img=new JLabel();
+		img.setSize(229, 186);
+		img.setLocation(47, 21);
+		img=CogerImagen(img);
+		panel.add(img);
 		
 		
+		setResizable(false);
 		
 		//Escuchadores
 		btnAceptar.addActionListener(new ActionListener() 
@@ -128,10 +125,7 @@ public class clsPaginaPrincipal extends JFrame {
 						g++;
 						
 						clsTablero frame = new clsTablero();
-//			            frame.createAndShowGUI(); 
 			            frame.setVisible(true);//necessary as of 1.3
-//			            frame.setSelected(true);
-			            
 			            txtContraseña.setText("");
 					}
 				}
@@ -170,53 +164,18 @@ public class clsPaginaPrincipal extends JFrame {
 
 		
 	}
+
+	
+	public JLabel CogerImagen(JLabel l)
+	{
+//		l.setIcon(new ImageIcon(new ImageIcon("C:/Users/ALUMNO/git/Ajedrez_Mariano/Ajedrez (Mariano)/src/img/pagina_principal.png").getImage().getScaledInstance(160, 140, Image.SCALE_DEFAULT)));
+		l.setIcon(new ImageIcon(new ImageIcon(getClass().getResource("/img/pagina_principal.png")).getImage().getScaledInstance(160, 140, Image.SCALE_DEFAULT)));
+
+		return l;
+	}
 }
 
 
 
 
 
-		
-//		if(txtNickname.getText().equals("administrador")&&txtContraseña.getText().equals("admi"))
-//		{
-//			g++;
-//			frmPrincipalAdministrador frame = new frmPrincipalAdministrador("Menú de administrador");
-//            frame.CreateAndShowGUI(); 
-//            frame.setVisible(true);//necessary as of 1.3
-//            frame.setSelected(true);
-//            
-//            
-//            txtContraseña.setText("");
-//            
-//			
-//		}
-//		
-//		else
-//		{
-//			for(clsUsuario aux:usus)
-//			{
-//				if((txtNickname.getText().equals(aux.getNickname()))&&(txtContraseña.getText().equals(aux.getContraseña())))
-//				{
-//					g++;
-//					
-//					frmPrincipalUsuario frame = new frmPrincipalUsuario("Menú de usuario");
-//		            frame.createAndShowGUI(); 
-//		            frame.setVisible(true);//necessary as of 1.3
-//		            frame.setSelected(true);
-//		            
-//		            txtContraseña.setText("");
-//				}
-//			}
-//			
-//			
-//			if(g==0)
-//			{
-//				JOptionPane.showMessageDialog(this, "Usuario o contraseña incorrectos");
-//			}
-//
-//			
-//			
-//			
-//		}
-//		
-//		break;
--- Ajedrez (Mariano)/src/clsMain.java
+++ Ajedrez (Mariano)/src/clsMain.java
@@ -11,7 +11,6 @@ public class clsMain {
 
 	public static void main(String[] args) 
 	{
-		System.out.println("Prueba");
 
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {

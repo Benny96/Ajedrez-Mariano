82f6cf3dcc3b1cdfd47b73fb8ba9802754340a61
ALUMNO <ALUMNO@192.168.1.109>
2016-11-17 16:21:23 +0100

    tablero visual y lógico

---
+++ Ajedrez (Mariano)/src/GUI/tablerologico.java
@@ -0,0 +1,719 @@
+package GUI;
+
+
+import java.awt.Color;
+import java.util.LinkedList;
+
+import LN.clsUsuario;
+
+public class tablerologico {
+
+	clsCasilla [][] tablero;
+	
+//	JLabel blanquito;
+//	JLabel nigga;
+//	
+//	JLabel ntiempo;
+//	
+//	JList asd;
+
+	public LinkedList<clsPieza> pblancas;
+	public LinkedList<clsPieza> pnegras;
+	LinkedList<clsCasilla> movact;
+	
+	clsCasilla acasilla;
+	clsCasilla ncasilla;
+	
+	clsRey reyb;
+	clsRey reyn;
+	
+	clsTorre btorrei;
+	clsTorre btorred;
+	clsTorre ntorrei;
+	clsTorre ntorred;
+	
+	Boolean turno;
+	
+	//private Runnable myTimer;
+	
+	int nmin;
+	int nseg;
+	private String nstr;
+
+	private int bmin;
+	private int bseg;
+	private String bstr;
+//	private JLabel btiempo;
+	
+	clsPieza selec;
+
+	private clsUsuario unigga;
+
+	private clsUsuario ublanco;
+	
+	public tablerologico()
+	{
+		tablero= new clsCasilla[8][8];
+		
+		pblancas= new LinkedList<clsPieza>();
+		pnegras= new LinkedList<clsPieza>();
+		movact= new LinkedList<clsCasilla>();
+		
+	}
+	public tablerologico(Boolean asd) 
+	{
+			
+		tablero= new clsCasilla[8][8];
+		
+		ublanco= new clsUsuario("blanquito","a","a","blanquito","a");
+		unigga= new clsUsuario("nigga","b","b","nigga","b");
+		
+		for(int i=0;i<8;i++)
+		{
+			for(int j=0;j<8;j++)
+			{
+				tablero[i][j]=new clsCasilla(i, j);		
+				
+			}
+		}
+		
+		turno=true;
+		
+		pblancas= new LinkedList<clsPieza>();
+		pnegras= new LinkedList<clsPieza>();
+		movact= new LinkedList<clsCasilla>();
+		
+		reyb=new clsRey(0,3,true);
+		reyn=new clsRey(7,3,false);
+		
+		btorrei=new clsTorre(0,7,true);
+		btorred=new clsTorre(0,0,true);
+		ntorrei=new clsTorre(7,7,false);
+		ntorred=new clsTorre(7,0,false);
+
+		pblancas.add(btorred);
+		pblancas.add(new clsCaballo(0,1,true));
+		pblancas.add(new clsAlfil(0,2,true));
+		pblancas.add(reyb);
+		pblancas.add(new clsReina(0,4,true));
+		pblancas.add(new clsAlfil(0,5,true));
+		pblancas.add(new clsCaballo(0,6,true));
+		pblancas.add(btorrei);
+		
+		pblancas.add(new clsPeon(1,0,true));
+		pblancas.add(new clsPeon(1,1,true));
+		pblancas.add(new clsPeon(1,2,true));
+		pblancas.add(new clsPeon(1,3,true));
+		pblancas.add(new clsPeon(1,4,true));
+		pblancas.add(new clsPeon(1,5,true));
+		pblancas.add(new clsPeon(1,6,true));
+		pblancas.add(new clsPeon(1,7,true));
+		
+		pnegras.add(ntorred);
+		pnegras.add(new clsCaballo(7,1,false));
+		pnegras.add(new clsAlfil(7,2,false));
+		pnegras.add(reyn);
+		pnegras.add(new clsReina(7,4,false));
+		pnegras.add(new clsAlfil(7,5,false));
+		pnegras.add(new clsCaballo(7,6,false));
+		pnegras.add(ntorrei);
+		
+		pnegras.add(new clsPeon(6,0,false));
+		pnegras.add(new clsPeon(6,1,false));
+		pnegras.add(new clsPeon(6,2,false));
+		pnegras.add(new clsPeon(6,3,false));
+		pnegras.add(new clsPeon(6,4,false));
+		pnegras.add(new clsPeon(6,5,false));
+		pnegras.add(new clsPeon(6,6,false));
+		pnegras.add(new clsPeon(6,7,false));
+		
+		for(clsPieza aux: pblancas)
+		{
+			tablero[aux.getY()][aux.getX()].setOcupado(aux);
+		}
+		for(clsPieza aux: pnegras)
+		{
+			tablero[aux.getY()][aux.getX()].setOcupado(aux);
+		}
+		
+		nmin=10;
+		nseg = 00;
+		nstr = String.format("%d:%02d", nmin, nseg);
+		
+		bmin=10;
+		bseg = 00;
+		bstr = String.format("%d:%02d", bmin, bseg);
+
+		
+		//para que no meleste
+//		myTimer = new Timer();
+//		
+//		Thread a= new Thread (myTimer);
+//		a.start();
+		
+		}	
+	
+
+	public tablerologico clonar(tablerologico tab)
+	{
+		tablerologico mewto = new tablerologico();
+		clsCasilla[][] tablero=mewto.getTablero();
+		
+//		LinkedList<clsPieza> piezas=new LinkedList<clsPieza>();
+//		
+//		piezas.addAll(tab.getPblancas());
+//		piezas.addAll(tab.getPnegras());
+		
+		for(clsPieza paux: tab.getPblancas())
+		{
+			mewto.getPblancas().add(paux.clonar(paux));
+		}
+		for(clsPieza paux: tab.getPnegras())
+		{
+			mewto.getPnegras().add(paux.clonar(paux));
+		}
+		
+		for(clsPieza aux: mewto.getPblancas())
+		{
+			tablero[aux.getY()][aux.getX()].setOcupado(aux);
+		}
+		for(clsPieza aux:mewto.getPnegras())
+		{
+			tablero[aux.getY()][aux.getX()].setOcupado(aux);
+		}
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		//clsPieza pieza= vtablero[t1][t2].getPieza();
+		
+		//ntablero[t1][t2].setOcupado(vtablero[t1][t2].getPieza());
+//		if(pieza instanceof clsPeon)
+//		{
+//			ntablero[t1][t2].setOcupado(new clsPeon(pieza.getY(),pieza.getX(),pieza.getColor()));
+//		}
+//		if(pieza instanceof clsTorre)
+//		{
+//			ntablero[t1][t2].setOcupado(new clsTorre(pieza.getY(),pieza.getX(),pieza.getColor()));
+//		}
+//		if(pieza instanceof clsCaballo)
+//		{
+//			ntablero[t1][t2].setOcupado(new clsCaballo(pieza.getY(),pieza.getX(),pieza.getColor()));
+//		}
+//		if(pieza instanceof clsAlfil)
+//		{
+//			ntablero[t1][t2].setOcupado(new clsAlfil(pieza.getY(),pieza.getX(),pieza.getColor()));
+//		}
+//		if(pieza instanceof clsRey)
+//		{
+//			ntablero[t1][t2].setOcupado(new clsRey(pieza.getY(),pieza.getX(),pieza.getColor()));
+//		}
+//		if(pieza instanceof clsReina)
+//		{
+//			ntablero[t1][t2].setOcupado(new clsReina(pieza.getY(),pieza.getX(),pieza.getColor()));
+//		}	
+		
+		return mewto;
+		
+	}
+	
+	public void clear(LinkedList <clsCasilla> borrar)
+	{
+		for(clsCasilla aux: borrar)
+		{
+			
+			if(aux.provisional)
+			{
+				System.out.println("ERTYUI");
+				int i=aux.getx();
+				int j=aux.gety();
+				aux.provisional=false;
+			if((i+j)%2==0)
+				tablero[j][i].setBackground(Color.WHITE);
+			else
+				tablero[j][i].setBackground(Color.GRAY);
+			}
+			else
+			{
+				if(aux.getOcupado()==null)
+				{
+			aux.mov=false;
+			aux.setIcon(null);
+			}
+			}
+		}
+		borrar.clear();
+	}
+	
+	public void revisar()
+	{
+		LinkedList<clsPieza> todas = null;
+		todas.addAll(pblancas);
+		todas.addAll(pnegras);
+		for(clsPieza pieza: todas)
+		{
+			for(clsCasilla casilla: pieza.getMovimientos())
+			{
+				
+			}
+		}
+	}
+	
+	public LinkedList<clsPieza> getpniegras()
+	{
+		return pnegras;
+	}
+	public Boolean comprobarjaque(clsRey rey,tablerologico tab)
+	{
+		LinkedList<clsPieza> colorcete;
+		
+		clsCasilla[][] tablero=tab.getTablero();
+		
+		
+		if(rey.getColor())
+			colorcete=tab.getPnegras();
+		else
+			colorcete=tab.getPblancas();
+		
+		rey.mov(tablero);//revisar a futuro
+		
+		for(clsPieza paux: colorcete )
+		{
+			paux.mov(tablero);
+			for(clsCasilla caux: paux.getMovimientos())
+			{
+				if(caux.equals(rey.sitio(tablero)))
+				{
+					return true;
+				}
+			}
+		}
+			return false;
+			
+		
+	}
+	public void dibujarmov(LinkedList<clsCasilla> lista)
+	{
+		for(clsCasilla aux: lista)
+		{
+		if(aux.getOcupado()==null)
+		{
+			aux.imov();
+			movact.add(aux);
+		}
+		else
+		{
+		aux.provisional=true;
+		aux.setBackground(Color.red);
+		movact.add(aux);
+		}
+		}
+	}
+	public void action(clsCasilla casilla) {
+		// TODO Auto-generated method stub
+		//2.comer al paso
+				//3. peon al final
+				//4.reloj 0 parar
+				//5.metodo terminar partida
+				//6. jaque
+				
+				 acasilla=ncasilla;
+				 ncasilla=casilla;
+				 
+				 System.out.println(ncasilla);
+
+				if(ncasilla.getOcupado()==null)
+				{
+					if(ncasilla.mov)
+					{
+						tablero[selec.getY()][selec.getX()].setOcupado(null);
+						if(selec.a.equals(Comun.clsConstantes.piezas.Rey))
+						{
+							if(selec.getPrimera()==false)
+								selec.setPrimera(true);
+							
+//							if(selec.getColor() && ncasilla.getx()==0 && ncasilla.gety()==1 && reyb.getPrimera()==true && btorred.getPrimera()==true && tablero[0][2].getOcupado()==null )
+//							{
+//								tablero[0][2].setOcupado(btorred);
+//								tablero[0][0].setOcupado(null);
+//							}
+							if(selec.getColor() && ncasilla.getx()==1 && ncasilla.gety()==0  )
+							{
+								tablero[0][2].setOcupado(btorred);
+								tablero[0][0].setOcupado(null);
+							}
+							if(selec.getColor() && ncasilla.getx()==5 && ncasilla.gety()==0  )
+							{
+								tablero[0][4].setOcupado(btorrei);
+								tablero[0][7].setOcupado(null);
+							}
+							
+							if(selec.getColor()==false && ncasilla.getx()==1 && ncasilla.gety()==7  )
+							{
+								tablero[7][2].setOcupado(ntorred);
+								tablero[7][0].setOcupado(null);
+							}
+							if(selec.getColor()==false && ncasilla.getx()==5 && ncasilla.gety()==7 )
+							{
+								tablero[7][4].setOcupado(ntorrei);
+								tablero[7][7].setOcupado(null);
+							}
+							
+						}
+						if(selec.a.equals(Comun.clsConstantes.piezas.Torre))
+						{
+							if(selec.getPrimera()==false)
+								selec.setPrimera(true);
+								
+						}
+						System.out.println("SDFGHJKHGHFDFHJ");
+						System.out.println(selec.sitio(tablero));
+						ncasilla.setOcupado(selec);
+						movact.remove(ncasilla);
+						if(selec.getColor())
+							reyn.jaque=comprobarjaque(reyn,this);
+						else
+							reyb.jaque=comprobarjaque(reyb,this);
+						selec=null;
+						clear(movact);
+						turno=!turno;
+					
+						//revisar();
+					}
+					clear(movact);
+				}
+				else if(ncasilla.getOcupado()!=null)
+				{
+					//clear(movact);
+					
+					if(ncasilla.getOcupado().a.equals(Comun.clsConstantes.piezas.Rey))
+					{
+						clear(movact);
+						selec=ncasilla.getOcupado();
+						selec.mov(tablero);
+						LinkedList<clsCasilla> lista= new LinkedList<clsCasilla>();
+						selec.mov(tablero);
+						lista.addAll(selec.movimientos);
+						
+						if(selec.getColor()== turno)
+						{	
+							System.out.println("DFGHJKLKGFGHJKkjvbvcvbnbvcvbnbvbnmnbv");
+							LinkedList<clsPieza> colorcete;
+							
+							if(selec.getColor())
+								colorcete=pnegras;
+							else
+								colorcete=pblancas;
+							
+								for(clsPieza pieza: colorcete)
+								{
+									for(clsCasilla caux: pieza.influencia(tablero))
+									{
+										lista.remove(caux);
+									}
+								}
+								dibujarmov(lista);
+							
+						}
+						
+					
+					}
+					else{
+						
+					if(ncasilla.provisional==false)
+					{
+					clear(movact);
+					
+					selec=ncasilla.getOcupado();
+					
+					if (selec.getColor()== turno)
+					{
+						selec.mov(tablero);
+						
+						LinkedList aux;
+						
+						
+						dibujarmov(selec.movimientos);
+					}
+
+					}
+					
+			
+			
+					else
+					{
+						if(selec.getColor())
+							pblancas.remove(ncasilla.getOcupado());
+						else
+							pnegras.remove(ncasilla.getOcupado());
+						
+						tablero[selec.getY()][selec.getX()].setOcupado(null);
+						ncasilla.setOcupado(selec);
+						ncasilla.provisional=false;
+						int i=ncasilla.getx();
+						int j=ncasilla.gety();
+					if((i+j)%2==0)
+						ncasilla.setBackground(Color.WHITE);
+					else
+						ncasilla.setBackground(Color.GRAY);
+					
+						movact.remove(ncasilla);
+						if(selec.getColor())
+							reyn.jaque=comprobarjaque(reyn,this);
+						else
+							reyb.jaque=comprobarjaque(reyb,this);
+						selec=null;
+						clear(movact);
+						turno=!turno;
+					//	revisar();
+					}
+				}
+				}
+			
+				System.out.println("el rey nigro");
+				System.out.println(reyn.jaque);
+				System.out.println("el rey blanco");
+				System.out.println(reyb.jaque);
+	}
+
+	
+//	private void Conversor()
+//	{
+//		if(turno)
+//		{
+//			bseg--;
+//			if (bseg==-1)
+//			{
+//				bseg=59;
+//				bmin--;
+//			}
+//			bstr = String.format("%d:%02d", bmin, bseg);
+//			//btiempo.setText(bstr);
+//		}
+//		else
+//		{
+//		nseg--;
+//		if (nseg==-1)
+//		{
+//			nseg=59;
+//			nmin--;
+//		}
+//		nstr = String.format("%d:%02d", nmin, nseg);
+//		//ntiempo.setText(nstr);
+//		}
+//	}
+	
+	
+//	class Timer implements Runnable
+//	{
+//		@Override
+//		public void run() 
+//		{
+//			while(true)
+//			{
+//					try 
+//					{
+//						Thread.sleep(1000);
+//						Conversor();
+//					}
+//					catch (InterruptedException e) 
+//					{
+//						return;
+//					}
+//			}
+//		}
+//	
+//	}
+//
+
+	public clsCasilla[][] getTablero() {
+		return tablero;
+	}
+
+	public void setTablero(clsCasilla[][] tablero) {
+		this.tablero = tablero;
+	}
+
+	public LinkedList<clsPieza> getPblancas() {
+		return pblancas;
+	}
+
+	public void setPblancas(LinkedList<clsPieza> pblancas) {
+		this.pblancas = pblancas;
+	}
+
+	public LinkedList<clsPieza> getPnegras() {
+		return pnegras;
+	}
+
+	public void setPnegras(LinkedList<clsPieza> pnegras) {
+		this.pnegras = pnegras;
+	}
+
+	public LinkedList<clsCasilla> getMovact() {
+		return movact;
+	}
+
+	public void setMovact(LinkedList<clsCasilla> movact) {
+		this.movact = movact;
+	}
+
+	public clsCasilla getAcasilla() {
+		return acasilla;
+	}
+
+	public void setAcasilla(clsCasilla acasilla) {
+		this.acasilla = acasilla;
+	}
+
+	public clsCasilla getNcasilla() {
+		return ncasilla;
+	}
+
+	public void setNcasilla(clsCasilla ncasilla) {
+		this.ncasilla = ncasilla;
+	}
+
+	public clsRey getReyb() {
+		return reyb;
+	}
+
+	public void setReyb(clsRey reyb) {
+		this.reyb = reyb;
+	}
+
+	public clsRey getReyn() {
+		return reyn;
+	}
+
+	public void setReyn(clsRey reyn) {
+		this.reyn = reyn;
+	}
+
+	public clsTorre getBtorrei() {
+		return btorrei;
+	}
+
+	public void setBtorrei(clsTorre btorrei) {
+		this.btorrei = btorrei;
+	}
+
+	public clsTorre getBtorred() {
+		return btorred;
+	}
+
+	public void setBtorred(clsTorre btorred) {
+		this.btorred = btorred;
+	}
+
+	public clsTorre getNtorrei() {
+		return ntorrei;
+	}
+
+	public void setNtorrei(clsTorre ntorrei) {
+		this.ntorrei = ntorrei;
+	}
+
+	public clsTorre getNtorred() {
+		return ntorred;
+	}
+
+	public void setNtorred(clsTorre ntorred) {
+		this.ntorred = ntorred;
+	}
+
+	public Boolean getTurno() {
+		return turno;
+	}
+
+	public void setTurno(Boolean turno) {
+		this.turno = turno;
+	}
+
+//	public Runnable getMyTimer() {
+//		return myTimer;
+//	}
+//
+//	public void setMyTimer(Runnable myTimer) {
+//		this.myTimer = myTimer;
+//	}
+
+	public int getNmin() {
+		return nmin;
+	}
+
+	public void setNmin(int nmin) {
+		this.nmin = nmin;
+	}
+
+	public int getNseg() {
+		return nseg;
+	}
+
+	public void setNseg(int nseg) {
+		this.nseg = nseg;
+	}
+
+	public String getNstr() {
+		return nstr;
+	}
+
+	public void setNstr(String nstr) {
+		this.nstr = nstr;
+	}
+
+	public int getBmin() {
+		return bmin;
+	}
+
+	public void setBmin(int bmin) {
+		this.bmin = bmin;
+	}
+
+	public int getBseg() {
+		return bseg;
+	}
+
+	public void setBseg(int bseg) {
+		this.bseg = bseg;
+	}
+
+	public String getBstr() {
+		return bstr;
+	}
+
+	public void setBstr(String bstr) {
+		this.bstr = bstr;
+	}
+
+	public clsPieza getSelec() {
+		return selec;
+	}
+
+	public void setSelec(clsPieza selec) {
+		this.selec = selec;
+	}
+
+	public clsUsuario getUnigga() {
+		return unigga;
+	}
+
+	public void setUnigga(clsUsuario unigga) {
+		this.unigga = unigga;
+	}
+
+	public clsUsuario getUblanco() {
+		return ublanco;
+	}
+
+	public void setUblanco(clsUsuario ublanco) {
+		this.ublanco = ublanco;
+	}
+}
---
+++ Ajedrez (Mariano)/src/GUI/tablerovisual.java
@@ -0,0 +1,274 @@
+package GUI;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedList;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+
+
+
+
+
+
+import LN.clsUsuario;
+
+public class tablerovisual extends JFrame implements ActionListener
+{
+	private static final long serialVersionUID = 1L;
+	
+	JPanel pPrincipal;
+	clsCasilla [][] tablero;
+	
+	tablerologico tab;
+	
+	JLabel blanquito;
+	JLabel nigga;
+	
+	JLabel ntiempo;
+	
+	JList asd;
+
+	public LinkedList<clsPieza> pblancas;
+	public LinkedList<clsPieza> pnegras;
+	LinkedList<clsCasilla> movact;
+	
+	clsCasilla acasilla;
+	clsCasilla ncasilla;
+	
+	clsRey reyb;
+	clsRey reyn;
+	
+	clsTorre btorrei;
+	clsTorre btorred;
+	clsTorre ntorrei;
+	clsTorre ntorred;
+	
+	Boolean turno;
+	
+	private Runnable myTimer;
+	
+	int nmin;
+	int nseg;
+	private String nstr;
+
+	private int bmin;
+	private int bseg;
+	private String bstr;
+	private JLabel btiempo;
+	
+	clsPieza selec;
+	
+	public tablerovisual() 
+	{
+	
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(0, 0, 1360, 720);
+		pPrincipal = new JPanel();
+		pPrincipal.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(pPrincipal);
+		pPrincipal.setLayout(null);
+		
+		tab= new tablerologico(true);
+		
+		
+		tablero= tab.getTablero();
+		
+
+		bseg=tab.getBseg();
+		bmin=tab.getBmin();
+		
+		nseg=tab.getNseg();
+		nmin=tab.getNmin();
+		
+		clsUsuario ublanco= tab.getUblanco();
+		clsUsuario unigga= tab.getUnigga();
+		
+		blanquito= new JLabel(ublanco.getNickname());
+		blanquito.setBounds(475, 595, 100,40);
+		
+		Font labelFont = blanquito.getFont();
+		String labelText =blanquito.getText();
+
+		int stringWidth = blanquito.getFontMetrics(labelFont).stringWidth(labelText);
+		int componentWidth = blanquito.getWidth();
+
+		// Find out how much the font can grow in width.
+		double widthRatio = (double)componentWidth / (double)stringWidth;
+
+		int newFontSize = (int)(labelFont.getSize() * widthRatio);
+		int componentHeight = blanquito.getHeight();
+
+		// Pick a new font size so it will not be larger than the height of label.
+		int fontSizeToUse = Math.min(newFontSize, componentHeight);
+
+		// Set the label's font size to the newly determined size.
+		blanquito.setFont(new Font(labelFont.getName(), Font.PLAIN, fontSizeToUse));
+		pPrincipal.add(blanquito);
+		
+		nigga= new JLabel(unigga.getNickname());
+		nigga.setBounds(80, 80, 100,40);
+		nigga.setFont(new Font(labelFont.getName(), Font.PLAIN, fontSizeToUse));
+		pPrincipal.add(nigga);
+	
+		for(int i=0;i<8;i++)
+		{
+			for(int j=0;j<8;j++)
+			{
+				tablero[i][j].setText(Integer.toString(i)+Integer.toString(j));
+				if((i+j)%2==0)
+					tablero[i][j].setBackground(Color.WHITE);
+				else
+					tablero[i][j].setBackground(Color.GRAY);			
+				
+				tablero[i][j].setBounds(500-j*60, 540-i*60, 60, 60);
+				tablero[i][j].addActionListener(this);
+				pPrincipal.add(tablero[i][j]);
+				
+			}
+		}
+		
+		turno=tab.getTurno();
+		
+		pblancas= tab.getPblancas();
+		pnegras= tab.getPnegras();
+		movact= tab.getMovact();
+		
+		reyb=tab.getReyb();
+		reyn=tab.getReyn();
+		
+		btorrei=tab.getBtorrei();
+		btorred=tab.getBtorred();
+		ntorrei=tab.getBtorrei();
+		ntorred=tab.getNtorred();
+		
+//		for(clsPieza aux: pblancas)
+//		{
+//			tablero[aux.getY()][aux.getX()].setOcupado(aux);
+//		}
+//		for(clsPieza aux: pnegras)
+//		{
+//			tablero[aux.getY()][aux.getX()].setOcupado(aux);
+//		}
+		
+		nstr = tab.getNstr();
+		
+		ntiempo= new JLabel(nstr);
+		ntiempo.setBounds(160, 80, 100,40);
+		ntiempo.setFont( new Font( "Arial", Font.BOLD, 18 ));
+		pPrincipal.add(ntiempo);
+		
+		
+		bstr = tab.getBstr();
+		
+		btiempo= new JLabel(bstr);
+		btiempo.setBounds(410, 595, 100,40);
+		btiempo.setFont( new Font( "Arial", Font.BOLD, 18 ));
+		pPrincipal.add(btiempo);
+		
+		//para que no meleste
+		myTimer = new Timer1();
+		
+		Thread a= new Thread (myTimer);
+		a.start();
+		
+		
+	}	
+	public void actionPerformed(ActionEvent arg) {
+		// TODO Auto-generated method stub
+		
+		//2.comer al paso
+		//3. peon al final
+		//4.reloj 0 parar
+		//5.metodo terminar partida
+		//6. jaque
+		
+		 acasilla=ncasilla;
+		 ncasilla=(clsCasilla) arg.getSource();
+		 
+		 System.out.println(ncasilla);
+
+		tab.action(ncasilla);
+		
+		this.repaint();
+		System.out.println("el rey nigro");
+		System.out.println(reyn.jaque);
+		System.out.println("el rey blanco");
+		System.out.println(reyb.jaque);
+	}
+
+	private void Conversor()
+	{
+		System.out.println("SDFGHJKL"+ turno);
+		turno=tab.getTurno();
+		if(turno)
+		{
+			bseg--;
+			if (bseg==-1)
+			{
+				bseg=59;
+				bmin--;
+			}
+			bstr = String.format("%d:%02d", bmin, bseg);
+			tab.setBstr(bstr);
+			btiempo.setText(bstr);
+		}
+		else
+		{
+		nseg--;
+		if (nseg==-1)
+		{
+			nseg=59;
+			nmin--;
+		}
+		nstr = String.format("%d:%02d", nmin, nseg);
+		tab.setNstr(nstr);
+		
+		ntiempo.setText(nstr);
+		}
+		
+		
+		this.repaint();
+	}
+	class Timer1 implements Runnable
+	{
+		@Override
+		public void run() 
+		{
+			while(true)
+			{
+					try 
+					{
+						Thread.sleep(1000);
+						Conversor();
+						
+					}
+					catch (InterruptedException e) 
+					{
+						return;
+					}
+					
+			}
+		}
+	
+	}
+	
+	}

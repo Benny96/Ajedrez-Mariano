25e6519be0c2f38a5cfcee43e22c2de367592814
Echever <imanolecheverria13@gmail.com>
2016-11-24 20:20:14 +0100

    no me dejaba desde eclipse

--- Ajedrez (Mariano)/src/GUI/clsPieza.java
+++ Ajedrez (Mariano)/src/GUI/clsPieza.java
@@ -13,10 +13,10 @@ public class clsPieza
 	piezas a;
 	private Boolean color; //True--> blanco ///  False--> negro
 	private ImageIcon icon;
-
 	protected LinkedList<GUI.clsCasilla> movimientos;
 	protected LinkedList<clsCasilla> influencia;
 	protected Boolean primera;
+	int valor;
 	
 	public clsPieza(int x, int y, Boolean color)
 	{
@@ -24,6 +24,8 @@ public class clsPieza
 		this.x=y;
 		this.color=color;	
 		this.primera=false;
+		
+		
 
 		movimientos= new LinkedList<clsCasilla>();
 		influencia= new LinkedList<clsCasilla>();
@@ -95,14 +97,13 @@ public class clsPieza
 	}
 	public LinkedList<clsCasilla> influencia(clsCasilla[][] tablero){
 		return movimientos;}
-	public LinkedList<clsCasilla> getMovimientos() {
+	public LinkedList<GUI.clsCasilla> getMovimientos() {
 		return movimientos;
 	}
 
-	public void setMovimientos(LinkedList<clsCasilla> movimientos) {
+	public void setMovimientos(LinkedList<GUI.clsCasilla> movimientos) {
 		this.movimientos = movimientos;
 	}
-
 
 	public LinkedList<clsCasilla> getInfluencia() {
 		return influencia;
@@ -119,35 +120,88 @@ public class clsPieza
 	public void setPrimera(Boolean primera) {
 		this.primera = primera;
 	}
-
 	
-	public clsPieza clonar(clsPieza pieza)
+	public clsPieza clonar(clsPieza pieza,tablerologico tab)
 	{
 		//mejorar rey,torre atributos especiales
+		clsCasilla [][] tablerete=tab.getTablero();
 		if(pieza instanceof clsPeon)
 		{
-			return (new clsPeon(pieza.getY(),pieza.getX(),pieza.getColor(),true));
+			clsPeon a=new clsPeon(pieza.getY(),pieza.getX(),pieza.getColor(),true);
+			for(clsCasilla s: pieza.movimientos)
+			{
+				a.movimientos.add(tablerete[s.gety()][s.gety()]);
+			}
+			return (a);
 		}
 		if(pieza instanceof clsTorre)
 		{
-			return(new clsTorre(pieza.getY(),pieza.getX(),pieza.getColor()));
+			clsTorre a=new clsTorre(pieza.getY(),pieza.getX(),pieza.getColor(),true);
+			for(clsCasilla s: pieza.movimientos)
+			{
+				a.movimientos.add(tablerete[s.gety()][s.gety()]);
+			}
+			return (a);
 		}
 		if(pieza instanceof clsCaballo)
 		{
-			return(new clsCaballo(pieza.getY(),pieza.getX(),pieza.getColor()));
+			clsCaballo a=new clsCaballo(pieza.getY(),pieza.getX(),pieza.getColor(),true);
+			for(clsCasilla s: pieza.movimientos)
+			{
+				a.movimientos.add(tablerete[s.gety()][s.gety()]);
+			}
+			return (a);
 		}
 		if(pieza instanceof clsAlfil)
 		{
-			return(new clsAlfil(pieza.getY(),pieza.getX(),pieza.getColor()));
+			clsAlfil a=new clsAlfil(pieza.getY(),pieza.getX(),pieza.getColor(),true);
+			for(clsCasilla s: pieza.movimientos)
+			{
+				a.movimientos.add(tablerete[s.gety()][s.gety()]);
+			}
+			return (a);
 		}
 		if(pieza instanceof clsRey)
 		{
-			return(new clsRey(pieza.getY(),pieza.getX(),pieza.getColor()));
+			clsRey a=new clsRey(pieza.getY(),pieza.getX(),pieza.getColor(),true);
+			for(clsCasilla s: pieza.movimientos)
+			{
+				a.movimientos.add(tablerete[s.gety()][s.gety()]);
+			}
+			return (a);
 		}
 		if(pieza instanceof clsReina)
 		{
-			return(new clsReina(pieza.getY(),pieza.getX(),pieza.getColor()));
+			clsReina a=new clsReina(pieza.getY(),pieza.getX(),pieza.getColor(),true);
+			for(clsCasilla s: pieza.movimientos)
+			{
+				a.movimientos.add(tablerete[s.gety()][s.gety()]);
+			}
+			return (a);
 		}	
 		return null;
+	}
+
+	public int getValor() {
+		return valor;
+	}
+
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+
+//	@Override
+//	protected Object clone() throws CloneNotSupportedException {
+//		// TODO Auto-generated method stub
+//		return super.clone();
+//	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		if(obj.getClass().equals(this.getClass())&& this.x==((clsPieza)obj).x &&  this.y==((clsPieza)obj).y )
+			return true;
+		else 
+			return false;
 	}
 }
